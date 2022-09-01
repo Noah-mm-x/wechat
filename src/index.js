9a6327f02c059e3f6b54820c09b3eea09b5f39e4
孟凡旭 <15733225953@163.com>
2022-09-01 11:08:41 +0800

    添加名单

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ bot.use(
     })
 )
 // 测试
+const roomList = ['机器人', '神经病四院', '保利时光印象二期3号楼']
 bot.use(
     WechatyFaceCartonPlugin({
         // apiKey: '89c866abdff27213baf122a7eb6684ca3a4f4041',
@@ -33,7 +34,7 @@ bot.use(
         secretId: 'AKIDEORL8qOE1c0A8tRksqbAVdYEanqgJjwy', // 腾讯secretId
         secretKey: 'shS2UsAICtOhorjI8nGbkkY8UebNyN0P', // 腾讯secretKey
         allowUser: [], // 允许哪些好友使用人像漫画化功能，为空[]代表所有人开启
-        allowRoom: ['机器人'], // 允许哪些群使用人像漫画化功能，为空[]代表不开启任何一个群
+        allowRoom: roomList, // 允许哪些群使用人像漫画化功能，为空[]代表不开启任何一个群
         quickModel: true, // 快速体验模式 默认关闭 开启后可直接生成二维码扫描体验，如果自己代码有登录逻辑可以不配置此项
         tipsword: '卡通', // 私聊发送消息，触发照片卡通化提示 如果直接发送图片，默认进入图片卡通化功能，不填则当用户初次发送文字消息时不做任何处理
     })
